f7bf5bc695be26ce08cd88e2e87021d434ce8e29
Grace Knox <gknox@graces-macbook-air.local>
2016-01-21 12:19:49 -0600

    Set up storyboard, added protocols.

--- College Profile Builder App/ViewController.swift
+++ College Profile Builder App/ViewController.swift
@@ -8,17 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    var colleges = ["University of Miami", "University of Colorado Boulder", "Colorado State"] 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 
 
 }
