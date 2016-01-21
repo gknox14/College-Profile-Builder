b5f46e9f3b9adf11f7209b0220aaf820f84d11c2
Grace Knox <gknox@graces-macbook-air.local>
2016-01-21 12:30:30 -0600

    Allow for entries to be deleted.

--- College Profile Builder App/ViewController.swift
+++ College Profile Builder App/ViewController.swift
@@ -11,15 +11,12 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
-    var colleges = ["University of Miami", "University of Colorado Boulder", "Colorado State"] 
+    var colleges = ["University of Miami", "University of Colorado Boulder", "Colorado State"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
     }
-    //func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    
-    //func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
-   
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return colleges.count
     }
@@ -28,6 +25,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let cell = tableView.dequeueReusableCellWithIdentifier("My Cell", forIndexPath: indexPath)
         cell.textLabel?.text = colleges[indexPath.row]
         return cell
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            colleges.removeAtIndex(indexPath.row)
+            tableView.reloadData()
+        }
     }
 
 }
