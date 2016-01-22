a9b0a9be1a0ce44d1bde2b8e2348128bbd03630c
Grace Knox <gknox@graces-macbook-air.local>
2016-01-22 12:08:19 -0600

    Allow for entries to be re ordered.

--- College Profile Builder App/ViewController.swift
+++ College Profile Builder App/ViewController.swift
@@ -12,11 +12,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var tableView: UITableView!
     var colleges = ["University of Miami", "University of Colorado Boulder", "Colorado State"]
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    //var cities : [City] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-    }
+        editButton.tag = 0
+       }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return colleges.count
     }
@@ -34,6 +37,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
     }
     
+    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        let college = colleges[sourceIndexPath.row]
+        colleges.removeAtIndex(sourceIndexPath.row)
+        colleges.insert(college, atIndex: destinationIndexPath.row)
+    }
+    
     @IBAction func onTappedAddButton(sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "Add College", message: nil, preferredStyle: .Alert)
         alert.addTextFieldWithConfigurationHandler { (textField) -> Void in
@@ -49,6 +62,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         alert.addAction(addAction)
         self.presentViewController(alert, animated: true, completion: nil)
         }
+    @IBAction func onTappedEditButton(sender: UIBarButtonItem) {
+        if sender.tag == 0 {
+            tableView.editing = true
+            sender.tag = 1
+        }
+        else {
+            tableView.editing = false
+            sender.tag = 0
+        }
+    }
+    
+    
     }
     
 
