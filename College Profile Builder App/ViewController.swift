e7bde21e452ad94b1c403392f08e7d67587f7eca
Grace Knox <gknox@graces-macbook-air.local>
2016-01-25 12:19:43 -0600

    Added segue to View Controller

--- College Profile Builder App/ViewController.swift
+++ College Profile Builder App/ViewController.swift
@@ -50,6 +50,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         colleges.insert(college, atIndex: destinationIndexPath.row)
     }
     
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     let dvc = segue.destinationViewController as! DetailViewController
+        let index = tableView.indexPathForSelectedRow?.row
+        dvc.college = colleges[index!]
+    }
+    
     @IBAction func onTappedAddButton(sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "Add College", message: nil, preferredStyle: .Alert)
         alert.addTextFieldWithConfigurationHandler { (textField) -> Void in
